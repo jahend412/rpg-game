37fa7feeea2585416ad9d4451e4e5eec9a2943e4
Josh Endemann <jahend44@gmail.com>
2023-08-13 14:19:33 -0500

    Add diceCount

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const hero = {
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
-    diceRoll: 6
+    diceRoll: 6,
+    diceCount: 3
 }
 
 const monster = {
@@ -11,23 +12,31 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: 4
+    diceRoll: 4,
+    diceCount: 1
 }
 
 
 
 function renderCharacter(data) {
-    const { elementId, name, health, avatar, diceRoll } = data
-    document.getElementById(elementId).innerHTML = `
-        <div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}"/>
-            <p class="health">health: <b> ${health} </b></p>
-            <div class="dice-container"><div class="dice"> ${diceRoll} </div></div>
+    const { elementId, name, health, avatar, diceRoll, diceCount } = data
+    let diceHtml = ""
+
+    for (let i = 0; i < diceCount; i++) {
+        diceHtml += `<div class="dice"> ${diceRoll} </div>`
+    }
+
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}" />
+        <div class="health">health: <b>${health}</b></div>
+        <div class="dice-container">
+            ${diceHtml}
         </div>
-    `
+    </div>`;
 }
 
-renderCharacter(hero)
-renderCharacter(monster)
+renderCharacter(hero);
+renderCharacter(monster);
 
